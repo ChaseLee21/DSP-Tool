d0c6edca5957821b9ae2e669da9f890361a5d9a1
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-31 16:06:44 -0700

    update: styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -77,32 +77,35 @@ function App() {
 
   return (
     <div className='container'>
-        <div className='row'>
-          <div className='col-auto'>
-            <button className='btn btn-primary' onClick={() => addTask()}>Add Task</button>
-          </div>
-          <div className='col-auto'>
-            <input id='addTask' className='form-control col-auto' type='text' onKeyUp={addTask}></input>
-          </div>
+      <div className='row'>
+        <h1>DSP Task List</h1>
+      </div>
+      <div className='row my-2'>
+        <div className='col-auto'>
+          <button className='btn btn-primary' onClick={() => addTask()}>Add Task</button>
         </div>
-        <ul id='taskList' className='list-group container'>
-          {tasks.map((task, index) => (
-            <li key={index} className='d-flex justify-content-between align-items-center list-group-item'>
-              <div className='d-flex align-items-center'>
-                <div className='d-flex flex-column'>
-                  <svg onClick={() => increaseTaskPriority(task)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
-                    <path  d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
-                  </svg>
-                  <svg onClick={() => decreaseTaskPriority(task)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
-                    <path d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"/>
-                  </svg>
-                </div>
-                <p className='ms-2 mb-0 h-auto'>{task}</p>
+        <div className='col-auto'>
+          <input id='addTask' className='form-control col-auto' type='text' onKeyUp={addTask}></input>
+        </div>
+      </div>
+      <ul id='taskList' className='list-group container'>
+        {tasks.map((task, index) => (
+          <li key={index} className='d-flex justify-content-between align-items-center list-group-item'>
+            <div className='d-flex align-items-center'>
+              <div className='d-flex flex-column'>
+                <svg onClick={() => increaseTaskPriority(task)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
+                  <path  d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
+                </svg>
+                <svg onClick={() => decreaseTaskPriority(task)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"/>
+                </svg>
               </div>
-              <button className='btn btn-sm btn-primary h-50' type='button' onClick={() => handleCompleteTask(task)}>Complete</button>
-            </li>
-          ))}
-        </ul>
+              <p className='ms-2 mb-0 h-auto'>{task}</p>
+            </div>
+            <button className='btn btn-sm btn-primary h-50' type='button' onClick={() => handleCompleteTask(task)}>Complete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
